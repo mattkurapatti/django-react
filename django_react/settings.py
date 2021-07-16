642bb2ed782b1a93f67599023f5ea68eb4ed6c4c
mattk <mk54@illinois.edu>
2021-07-16 19:09:37 -0400

    Updated settings.py

--- django_react/settings.py
+++ django_react/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'movie_project',
         'USER': 'root',
-        'PASSWORD': 'zvrPri3352',
+        'PASSWORD': '*',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
