d44e498847b597ee9a26df94cd8d9b91e6aac1c4
mattk <mk54@illinois.edu>
2021-07-16 19:06:15 -0400

    Cleaned up and commented scripts

--- movies/scripts/keywords_to_mysql.py
+++ movies/scripts/keywords_to_mysql.py
@@ -1,44 +1,38 @@
 import json
 import xlrd
+
 from movies.models import Movie, Keyword
 # Open the workbook and define the worksheet
 book = xlrd.open_workbook("keywords.xlsx")
 sheet = book.sheet_by_name("keywords")
 
-# Create a For loop to iterate through each row in the XLS file, starting at row 2 to skip the headers
+# Create a For loop to iterate through each row in the XLS file
 for r in range(1, sheet.nrows):
     if r % 5000 == 0:
         print(r)
     mov_id = int(sheet.cell(r, 0).value)
+    # check if movie exists
     try:
         movie = Movie.objects.get(movie_id=mov_id)
     except Exception:
         continue
     raw_keywords = str(sheet.cell(r, 1).value)
-    # print(raw_keywords)
+    # try to load into json
     try:
         json_keywords = json.loads(raw_keywords)
     except json.JSONDecodeError:
         print(raw_keywords)
         continue
     for keyword in json_keywords:
-        # keyword_id = keyword['id']
         wrd = keyword['name']
+        # get keyword if it already exists, otherwise create new keyword
         if Keyword.objects.filter(word=wrd).exists():
             keyword = Keyword.objects.get(word=wrd)
         else:
             keyword = Keyword(word=wrd)
             keyword.save()
+        # add movie-keyword relationship
         keyword.movies.add(movie)
 
+print("All Done!")
 
-
-    # Assign values from each row
-
-# Print results
-print("")
-print("All Done! Bye, for now.")
-print("")
-columns = str(sheet.ncols)
-rows = str(sheet.nrows)
-# print ("I just imported " %2B columns %2B " columns and " %2B rows %2B " rows to MySQL!")
